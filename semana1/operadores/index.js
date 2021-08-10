36ddabc96064789447e714d5713b5084b77bab2e
Patriciabadez <patriciabaldez1985@gmail.com>
2021-08-10 17:27:52 -0300

    semana1-aula04

---
+++ semana1/operadores/index.js
@@ -0,0 +1,27 @@
+/// EXERCÍCIOS DE INTERPRETAÇÃO
+
+/* Exercício 1
+const bool1 = true
+const bool2 = false
+const bool3 = !bool2 //true
+
+let resultado = bool1 && bool2
+// true && false => false!
+console.log("a. ", resultado) // a. false
+
+resultado = bool1 && bool2 && bool3 
+// true && false && true => false && true => false
+console.log("b. ", resultado) // b. false
+
+resultado = !resultado && (bool1 || bool2) 
+// true && (true || false) => true && true => true
+console.log("c. ", resultado) // c. true
+
+console.log("d. ", typeof resultado) // d. boolean
+*/
+
+/* Exercício 2
+ O código não está fazendo a soma pois os valores recebidos não sao números, mas sim strings!
+ Assim sendo, o console.log apenas junta os dois números
+*/
+
