006d3845eb1ad42665070b77c9edc2441d779b50
Patriciabadez <patriciabaldez1985@gmail.com>
2021-08-18 20:50:14 -0300

    semana2-aula09

--- semana2/condicionais/index.js
+++ semana2/condicionais/index.js
@@ -1,4 +1,6 @@
-// 1)const respostaDoUsuario = prompt("Digite o número que você quer testar")
+// 1) EXERCICIO DE INTERPRETAÇÃO DE CODIGO:
+
+//const respostaDoUsuario = prompt("Digite o número que você quer testar")
 // const numero = Number(respostaDoUsuario)
 
 // if (numero % 2 === 0) {
@@ -7,13 +9,15 @@
 //   console.log("Não passou no teste.")
 // }
 
-// a) Explique o que o código faz. Qual o teste que ele realiza? ele testa se passou ou não no teste, 
-
-// b) Para que tipos de números ele imprime no console "Passou no teste"? 
-
+// a) Explique o que o código faz. Qual o teste que ele realiza? O progrma testa se os numerois são pares
+// b) Para que tipos de números ele imprime no console "Passou no teste"? Para os numeros pares.
 // c) Para que tipos de números a mensagem é "Não passou no teste"?
-//_______________________________________________________________________________________________________________
-// 2)let fruta = prompt("Escolha uma fruta")
+// para os numeros impares.
+//________________________________________________________________________________________________________
+
+// 2)EXERCICIO DE INTERPRETAÇÃO DE CODIGO:
+
+//let fruta = prompt("Escolha uma fruta")
 // let preco
 // switch (fruta) {
 //   case "Laranja":
@@ -41,9 +45,11 @@
 //O preço da fruta  maçã  é  R$  5
 
 // c) Considere que um usuário queira comprar uma `Pêra`, qual seria a mensagem impressa no console se retirássemos o `break` que está logo acima do `default` (o `break` indicado pelo comentário "BREAK PARA O ITEM c.")? 
-//O preço da fruta  pera  é  R$  5
+//O preço da fruta  pera  é  R$  5,5
+//_____________________________________________________________________________________________________________
 
-//3)______________________________________________________________________________________________________________
+//3) EXERCICIO DE INTERPRETAÇÃO DE CODIGO:
+
 // const numero = Number(prompt("Digite o primeiro número."))
 
 // if(numero > 0) {
@@ -53,7 +59,6 @@
 
 // console.log(mensagem)
 
-//__________________________________________________________________________________________________________
 // a) O que a primeira linha está fazendo? 
 //Esta pedindo para o usuario digitar um numero..
 
@@ -63,7 +68,9 @@
 // c) Haverá algum erro no console? Justifique usando os conceitos de bloco ou escopo. faltou informar else, pois so esta lendo se for >0;
 
 //__________________________________________________________________________________________________________
-//1)
+
+//1)EXERCICIO DE ESCRITA DE CODIGO:
+
 // a) Faça um `prompt` para receber a idade do usuário e guarde em uma variável.
 
 // b) Garanta que essa variável é do tipo `Number`, você deve usar o cast para number para isso.
@@ -73,14 +80,15 @@
 // const idadeDoUsuario = prompt("Digite sua idade")
 // const idade = Number(idadeDoUsuario)
 
-// if (idade > 18) {
+// if (idade > = 18) {
 //   console.log("Você pode dirigir.")
 // } else {
 //   console.log("Você não pode dirigir.")
+//__________________________________________________________________________________________________________
 
-//2)_________________________________________________________________
-    
-// const turnoAlunoEstuda = prompt ("Em qual turno você estuda? Didite M para manhã/T para tarde/N para noite").toLowerCase()
+//2)EXERCICIO DE ESCRITA DE CODIGO:
+
+ // const turnoAlunoEstuda = prompt ("Em qual turno você estuda? Didite M para manhã/T para tarde/N para noite").toLowerCase()
 // if (turnoAlunoEstuda === "m"){
 //   console.log ("Bom dia!")
 // }else if(turnoAlunoEstuda === "v"){
@@ -90,27 +98,48 @@
 // }else {
 //     console.log ("Digite um turno válido")
 // }
-// horarios(TurnoAlunoEstuda)
-  
-//3)_________________________________________________________________
+// horarios(TurnoAlunoEstuda)__________________________________________________________________________________________________________
 
+//3)EXERCICIO DE ESCRITA DE CODIGO:
 
-const turnoAlunoEstuda = prompt ("Em qual turno você estuda? Didite M para manhã/T para tarde/N para noite").toLowerCase()
-switch (turnoAlunoEstuda){
-case  "m":
-  console.log ("Bom dia!")
-  break
-case "v":
-    console.log ("Boa tarde!")
-    break
-case "n":
-    console.log ("Boa noite!")
-    break
-// case"":
-//     console.log ("Digite um turno válido")
+// const turnoAlunoEstuda = prompt ("Em qual turno você estuda? Didite M para manhã/T para tarde/N para noite").toLowerCase()
+// switch (turnoAlunoEstuda){
+// case  "m":
+//   console.log ("Bom dia!")
+//   break
+// case "v":
+//     console.log ("Boa tarde!")
 //     break
-//     default
-}
-console.log ("Digite um turno válido")
+// case "n":
+//     console.log ("Boa noite!")
+//     break
+//     default:
 
-  
+// console.log ("Digite um turno válido!")
+// break
+// }
+
+//__________________________________________________________________________________________________________
+
+//4)EXERCICIO DE ESCRITA DE CODIGO:
+
+// Considere a situação: você vai ao cinema com um amigo ou amiga, porém ele/ela só assistirá a um filme com você se ele for do gênero fantasia e se o ingresso está abaixo de 15 reais. Faça um código que pergunta ao usuário qual o gênero de filme que vão assistir e outra pergunta sobre o preço do ingresso, então verifique se seu amigo ou amiga vai topar assistir o filme. Caso positivo, imprima no console a mensagem: "Bom filme!", caso contrário, imprima "Escolha outro filme :("
+
+// const generoFilme = prompt ("Qual gênero do filme")
+// const precoIngressoFilme = prompt ("Qual o preço do filme")
+
+// if (generoFilme === "fantasia" && precoIngressoFilme <= 15){
+// console.log ("Bom filme")                                           
+// }else{
+
+//     console.log ("Escolha outro filme :( ")
+// }
+//__________________________________________________________________________________________________________
+
+//DESAFIO:
+
+//__________________________________________________________________________________________________________
+
+//1)DESAFIO;
+
+
