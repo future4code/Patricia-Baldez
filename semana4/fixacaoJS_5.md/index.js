17b31a2d95800b71d5a97e981f3b734bc7946be5
Patriciabadez <patriciabaldez1985@gmail.com>
2021-09-01 21:10:03 -0300

    responsividade do site labetube

---
+++ semana4/fixacaoJS_5.md/index.js
@@ -0,0 +1,16 @@
+function criarArrayNomesAnimais() {
+    const animais = [
+      { nome: "Cachorro", classificacao: "mamífero" },
+      { nome: "Papagaio", classificacao: "ave" },
+      { nome: "Gato", classificacao: "mamífero" },
+      { nome: "Carpa", classificacao: "peixe" },
+      { nome: "Pomba", classificacao: "ave" }
+    ]
+
+ // Escreva seu código aqui
+  const novoArray = animais.map((bichinho)=>{
+    return bichinho.nome
+  })
+  
+  return novoArray
+}
