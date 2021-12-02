9425e9b8e18ad0a56c038fc704452c9a4a943d2f
Patricia Dias Baldez <patriciabaldez1985@gmail.com>
2021-12-02 18:55:09 -0300

    semana17-aula50

---
+++ semana16/Aula 47 - Knex.js/src/index.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express"
+
+// Esse arquivo seria o index.ts
+
+const getActorById = async (id: string): Promise<any> => {
+  const result = await connection.raw(`
+    SELECT * FROM Actor WHERE id = '${id}'
+  `)
+
+	return result[0][0]
+}
+
+// Assim a chamada funciona fora dos endpoints com .then()/.catch
+getActorById("001")
+	.then(result => {
+		console.log(result)
+	})
+	.catch(err => {
+		console.log(err)
+	});
+
+// Assim a chamada funciona fora dos endpoints com await
+(async () => {
+  console.log(await getActorById("001") )
+})()
+
+// Ou então podemos chamá-la dentro de um endpoint
+app.get("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id
+
+    console.log(await getActorById(id))
+
+    res.end()
+  } catch (error) {
+		console.log(error.message)
+    res.status(500).send("Unexpected error")
+  }
+}) // bata no http://localhost:3003/users/001 e veja o que acontece no terminal
