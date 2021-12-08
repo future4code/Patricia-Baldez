4fe3ae3a1909d3113903ffa2ac163708ed2d0ac6
Patricia Dias Baldez <patriciabaldez1985@gmail.com>
2021-12-08 16:33:24 -0300

    setup

---
+++ Labenu-System/src/index.ts
@@ -0,0 +1,37 @@
+import express, { Express, Request, Response } from "express";
+import knex from "knex";
+import cors from "cors";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+
+dotenv.config();
+
+export const connection = knex({
+	client: "mysql",
+	connection: {
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  }
+});
+
+const app: Express = express();
+app.use(express.json());
+app.use(cors());
+
+//somente para teste se esta funcionando:
+
+app.get("/", (req: Request, res: Response) =>{
+res.send("Ola pessoas!")
+})
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+       const address = server.address() as AddressInfo;
+       console.log(`Server is running in http://localhost: ${address.port}`);
+    } else {
+       console.error(`Failure upon starting server.`);
+    }
+});
