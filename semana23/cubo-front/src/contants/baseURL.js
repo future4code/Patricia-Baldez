765cb921642087019dc200b97d98fdd0c24d8efe
Patrícia Dias Baldez <88301906+Patriciabadez@users.noreply.github.com>
2022-02-10 15:37:38 -0300

    Delete semana23/cubo-front directory

--- semana23/cubo-front/src/contants/baseURL.js
+++
@@ -1,1 +0,0 @@
-export const baseURL = "https://case-cubo-backend.herokuapp.com"
--- semana23/cubo-front/src/page/Home/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const SectionData = styled.section`
-    display: flex;
-    align-items: flex-start;
-    justify-content: space-around;
-    padding-bottom: 2rem;
-`
